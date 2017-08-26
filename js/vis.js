ce9d11e93d50e87d3c4a91b2c0fafd144881e0b9
Nataliya Stepanova <gnykka@gmail.com>
2017-08-27 02:34:37 +0300

    Show more general statistics: list of companies and roles

--- js/vis.js
+++ js/vis.js
@@ -15,17 +15,38 @@
     return country;
   };
 
+  rivets.formatters.companies = function (values, showAll) {
+    if (!values) {
+      return '';
+    }
+    if (showAll) {
+      return values.join(', ');
+    }
+    return values.slice(0, 5).join(', ');
+  };
+
+  rivets.formatters.roles = function (values, showAll) {
+    if (!values) {
+      return '';
+    }
+    if (showAll) {
+      return values
+        .map((val) => `${val.name} (${val.count})`)
+        .join(', ');
+    }
+    return values
+      .slice(0, 3)
+      .map((val) => `${val.name} (${val.count})`)
+      .join(', ');
+  };
+
   document.addEventListener('DOMContentLoaded', () => {
     const dataContainer = document.getElementById('data');
     const statistics = {};
-    const loaded = true;
 
     rivets.bind(
       dataContainer,
-      {
-        loaded,
-        statistics
-      }
+      {statistics}
     );
 
     const onSelectCountry = (id) => {
@@ -48,13 +69,36 @@
         max: 132000,
         average: 69000,
       };
+      statistics.companies = {
+        values: ['Apple', 'Microsoft', 'IBM', 'Google', 'Amazon', 'Abbyy', 'Skype', 'Facebook'],
+        showAll: false
+      };
+      statistics.roles = {
+        values: [
+          {name: 'Backend Developer', count: 12},
+          {name: 'Frontend Developer', count: 11},
+          {name: 'Other', count: 7},
+          {name: 'System Administrator', count: 5},
+          {name: 'Full Stack Developer', count: 4},
+          {name: 'DevOps', count: 2},
+          {name: 'Data Analyst', count: 1}
+        ],
+        showAll: false
+      };
+      statistics.onShowAllCompanies = onShowAllCompanies;
+      statistics.onShowAllRoles = onShowAllRoles;
+      statistics.loaded = true;
 
       initSparkline('net-salary', statistics.netSalary, statistics.grossSalary);
       initSparkline('gross-salary', statistics.grossSalary, statistics.netSalary);
     };
 
-      //jobTitles,
-      //companies
+    const onShowAllCompanies = () => {
+      statistics.companies.showAll = true;
+    };
+    const onShowAllRoles = () => {
+      statistics.roles.showAll = true;
+    };
 
     initWorldMap(onSelectCountry);
 
